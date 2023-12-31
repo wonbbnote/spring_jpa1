c0ca8509006f848c40fb46857a4359e812deb480
heewon Choi <wonbb3313@gmail.com>
2023-12-31 13:09:48 +0900

    Feat: Add MemberServiceTest

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -1,12 +1,12 @@
 package jpabook.jpashop.service;
 
-import jakarta.transaction.Transactional;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.repository.MemberRepository;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
